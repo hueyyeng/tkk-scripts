fd7096a748a1df7585dcb3bcf0a3b2a7d64100e4
hueyyeng <huey.yeng.mmu@gmail.com>
2018-06-29 15:18:23 +0800

    Add new AI script

---
+++ Illustrator/Scripts/SaveAll.jsx
@@ -0,0 +1,22 @@
+#target Illustrator
+
+// Save All Opened Documents
+for (i=0; i<app.documents.length; i++)  
+    {  
+        var idoc = app.documents[i];  
+        //alert("+"+idoc.path+"+");  
+        if (idoc.path == "")  
+            {  
+                file = File.saveDialog ("Save file...");  
+                idoc.saveAs (file);  
+            }  
+        else  
+            {  
+                //alert(idoc.name);  
+                if (!idoc.saved)  
+                    {   
+                        idoc.save();  
+                        idoc.saved = true;  
+                    }  
+            }  
+    }  
